225a543956cd87a8ad3a1fa9718bcf7498a6b4a1
Sergej Charskij <sergej.charskij@gmail.com>
2017-05-06 20:19:12 +0100

    added user and profile routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,26 @@
 var router = require('express').Router();
 
+function combineErrors(err) {
+    return Object.keys(err.errors).reduce((errors, key) => {
+        errors[key] = err.errors[key].message;
+        return errors;
+    }, {});
+}
+
+/**
+ * Middleware for handling validation errors
+ */
+router.use((err, req, res, next) => {
+    if(err.name !== 'ValidationError') {
+        return next(err);
+    }
+
+    return res.status(422).json({
+        errors: combineErrors(err)
+    });
+});
+
+router.use('/', require('./users'));
+router.use('/profiles', require('./profiles'));
+
 module.exports = router;
